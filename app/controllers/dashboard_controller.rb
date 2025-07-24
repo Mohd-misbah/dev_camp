5f789b227e83d2cf35c88b927f0a237b28cdab96
mohd misbah <mohdmisbah2002@gmail.com>
2025-07-24 11:09:43 +0530

    initial mesg

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,7 @@
+class DashboardController < ApplicationController
+  def main
+  end
+
+  def user
+  end
+end
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+    class PagesController < ApplicationController
+        def home 
+        
+        end
+        def about
+
+        end
+    def something
+    @else = params[:else]  
+    @another_one = params[:another_one]  
+    
+    end
+
+    end
